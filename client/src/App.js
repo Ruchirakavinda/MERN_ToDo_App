4a85ed63e77e4279ebff78c2d8071f44614b981b
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-07-15 17:54:16 +0530

    Prepare the form for project

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Preloader from './components/Preloader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <i className="material-icons prefix">title</i>
+              <input id="icon_prefix" type="text" className="validate" />
+              <label htmlFor="icon_prefix">Title</label>
+            </div>
+            <div className="input-field col s6">
+              <i className="material-icons prefix">description</i>
+              <input id="icon_telephone" type="tel" className="validate" />
+              <label htmlFor="icon_telephone">Content</label>
+            </div>
+          </div>
+        </form>
+        
+        <Preloader/>
+
+        
+      <div class="collection">
+        <a href="#!" class="collection-item">Alvin</a>
+        <a href="#!" class="collection-item active">Alvin</a>
+        <a href="#!" class="collection-item">Alvin</a>
+        <a href="#!" class="collection-item">Alvin</a>
+      </div>
+            
+      </div>
     </div>
+  
+
   );
 }
 
