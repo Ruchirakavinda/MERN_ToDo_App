080f0d9b4e9a1bed2ede6d8bc4e446f6b71e2679
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-07-15 21:07:55 +0530

    Create a new todo by using the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { readTodos } from './functions';
 import Preloader from './components/Preloader';
+import { createTodos } from './api';
 
 function App() {
+
+  const [todo, setTodo] = useState(
+    {
+      title : '',
+      content : ''
+    }
+  )
 
   useEffect(() =>{
     const fetchdata = async()=>{
@@ -13,21 +21,43 @@ function App() {
     fetchdata()
   }, [])
 
+  const  onSubmitHandler =  async (e)=>{
+    e.preventDefault();
+    const result =  await createTodos(todo)
+    console.log(result)
+  }
+
+
   return (
     <div className="container">
       <div className="row">
-        <form className="col s12">
+
+        <pre>{JSON.stringify(todo)}</pre>
+        
+        <form className="col s12" onSubmit={onSubmitHandler}>
           <div className="row">
             <div className="input-field col s6">
               <i className="material-icons prefix">title</i>
-              <input id="icon_prefix" type="text" className="validate" />
+              <input id="icon_prefix" type="text" className="validate"
+              onChange={e=>setTodo({
+                ...todo,
+                title: e.target.value
+              })} />
               <label htmlFor="icon_prefix">Title</label>
             </div>
             <div className="input-field col s6">
               <i className="material-icons prefix">description</i>
-              <input id="icon_telephone" type="tel" className="validate" />
+              <input id="icon_telephone" type="tel" className="validate" 
+              onChange={e=>setTodo({
+                ...todo,
+                content: e.target.value
+              })}/>
               <label htmlFor="icon_telephone">Content</label>
             </div>
+          </div>
+
+          <div className="row right-align ">
+            <button className="waves-effect waves-light btn">Submit</button>
           </div>
         </form>
 
